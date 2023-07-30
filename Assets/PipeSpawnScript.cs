28f5936794ab55db86699bc17fb716ad7fd56236
Daim Bin Khalid <daimbkhalid@gmail.com>
2023-07-30 15:06:00 +0500

    pipe spawning

---
+++ Assets/PipeSpawnScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawnScript : MonoBehaviour
+{
+    public GameObject pipe;
+    public float spawnRate = 2;
+    private float timer = 0;
+    public float heightOffset = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPipe();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            SpawnPipe();
+            timer = 0;
+        }
+    }
+
+    void SpawnPipe()
+    {
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+
+        float x = transform.position.x;
+        float y = Random.Range(lowestPoint, highestPoint);
+        Instantiate(pipe, new Vector3(x, y, 0), transform.rotation);
+    }
+}
